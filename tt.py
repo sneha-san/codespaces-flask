509c51557a8cd2336bde77292db34b82e278f9ef
sneha-san <68964465+sneha-san@users.noreply.github.com>
2023-05-02 16:52:30 +0000

    added css again

--- tt.py
+++ tt.py
@@ -21,7 +21,7 @@ app.layout = html.Div([
     
     html.Div(className='row', children=[
         html.Div(className='six columns', children=[
-        dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+        dash_table.DataTable(data=df.to_dict('records'), page_size=10, style_table={'overflowX': 'auto'})
         ]),
         html.Div(className='six columns', children=[
         dcc.Graph( figure={}, id='controls-and-graph')
